984285bd92f6af51680a7f04e30880f8f34d61be
44mcr <00001082931263sp@al.educacao.sp.gov.br>
2024-06-11 11:28:38 -0300

    adicionando arquivo js css

---
+++ style.css
@@ -0,0 +1,52 @@
+function setup() {
+    createCanvas(400, 400)
+  }
+  
+  function draw() {
+    background(220);
+  }
+  function setup() {
+      createCanvas(600, 400);
+  }
+  
+  function draw() {
+      background(0);
+      circle(0, 0, 50);
+  }
+  let xBolinha = 300;
+  let yBolinha = 200;
+  let diametro = 15;
+  
+  function setup() {
+      createCanvas(600, 400);
+  }
+  
+  function draw() 
+      background(0);
+      circle(xBolinha, yBolinha, diametro);
+      xBolinha = xBolinha + 1; 
+  let velocidadeXBolinha = 6;
+  let velocidadeYBolinha = 6;
+  
+  function setup() {
+      createCanvas(600, 400);
+  }
+  
+  function draw() {
+      background(0);
+      circle(xBolinha, yBolinha, diametro);
+      xBolinha += velocidadeXBolinha;
+      yBolinha += velocidadeYBolinha;
+  }
+  function draw() {
+      background(0);
+      circle(xBolinha, yBolinha, diametro);
+      xBolinha += velocidadeXBolinha;
+      //yBolinha += velocidadeYBolinha;
+      
+      if (xBolinha > width) {
+          velocidadeXBolinha *= -1;
+      }
+  }
+  
+  
